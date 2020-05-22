ad9f55194edac39ff684bf982da47c98e4483796
Jacob Galam <hgecdkto@gmail.com>
2020-05-22 14:26:36 +0300

    add module.exports

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,17 @@
-const Router = require('express').Router();
+const router = require('express').Router();
 let User = require('../models/user');
 
 function returnError(res, err) {
   res.status(400).json('Error: ' + err);
 }
 
-Router.route('/').get((req, res) => {
+router.route('/').get((req, res) => {
   User.find()
     .then((users) => res.json(users))
     .catch(returnError(res, err));
 });
 
-Router.route('/add').post((req, res) => {
+router.route('/add').post((req, res) => {
   const username = req.body.username;
   const newUser = new User({ username });
 
@@ -21,3 +21,5 @@ Router.route('/add').post((req, res) => {
     })
     .catch(returnError(res, err));
 });
+
+module.exports = router;
