d9bd8371765b4272c2ed2cd435bf867c25c5ab57
Jacob Galam <hgecdkto@gmail.com>
2020-05-24 08:31:32 +0300

    add atacks to mongodb

--- backend/models/attack.js
+++ backend/models/attack.js
@@ -2,30 +2,59 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+var DEFAULT_VALUE = 'NA',
+
 const attackSchema = new Schema(
   {
     name: {
       type: String,
+      default: DEFAULT_VALUE,
     },
     description: {
       type: String,
+      default: DEFAULT_VALUE,
     },
     id: {
       type: String,
+      unique: true,
+      default: DEFAULT_VALUE,
     },
     x_mitre_platforms: {
-      type: String,
+      type: [String],
+      default: DEFAULT_VALUE,
     },
     x_mitre_detection: {
       type: String,
+      default: DEFAULT_VALUE,
     },
     phase_name: {
       type: String,
+      default: DEFAULT_VALUE,
     },
   },
   { timestamps: true }
 );
 
 const Attack = mongoose.model('Attack', attackSchema);
+
+function addAttacks(path) {
+  const data = require(path);
+
+  for (var i = 0; i < data.length; i++) {
+    var obj = data[i];
+    // phase_name
+
+    var attack = new Attack(obj.objects[0]);
+    const phase_name = obj.objects[0].kill_chain_phases[0].phase_name;
+    attack.phase_name = phase_name;
+    attack.save((err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }
+}
+
+addAttacks('../merged_file.json');
 
 module.exports = Attack;
