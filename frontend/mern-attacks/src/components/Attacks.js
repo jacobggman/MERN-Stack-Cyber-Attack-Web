2395fa4641396129d218354704d11176ce494532
Jacob Galam <hgecdkto@gmail.com>
2020-05-25 21:35:38 +0300

    fixing attack promise and add read more

--- frontend/mern-attacks/src/components/Attacks.js
+++ frontend/mern-attacks/src/components/Attacks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -8,11 +8,11 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
 import axios from 'axios';
+import ShowMoreText from 'react-show-more-text';
 
 // todo:
 
 // must - 4h
-// fix promise of the attack function - 2h
 // search description - 2h
 
 // bonus - 3h
@@ -63,59 +63,69 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// id, description, x_mitre_platforms, x_mitre_detection, phase_name
-export default function Attacks() {
-  const classes = useStyles();
+async function getAttacks() {
   const config = { headers: { 'Content-Type': 'application/json' } };
   config.headers['x-auth-token'] = localStorage.getItem('token');
-  let returnData = axios
-    .get('http://localhost:2802/attacks', config)
-    .then((res) => {
-      console.log(res.data);
-      const rows = res.data;
-      return (
-        <React.Fragment>
-          <Title>Recent Attacks</Title>
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell>Id</TableCell>
-                <TableCell>Description</TableCell>
-                <TableCell>x_mitre_platforms</TableCell>
-                <TableCell>x_mitre_detection</TableCell>
-                <TableCell>phase_name</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.id}>
-                  <TableCell>{row.id}</TableCell>
-                  <TableCell>{row.description}</TableCell>
-                  <TableCell>{row.x_mitre_platforms}</TableCell>
-                  <TableCell>{row.x_mitre_detection}</TableCell>
-                  <TableCell>{row.phase_name}</TableCell>
-                  <TableCell align="right">{row.amount}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <div className={classes.seeMore}>
-            <Link color="primary" href="#" onClick={preventDefault}>
-              See more attacks
-            </Link>
-          </div>
-        </React.Fragment>
-      );
-    })
-    .catch((err) => {
-      if (err.response !== undefined) {
-        console.log(err.response.data);
-        alert(err.response.data);
-      } else {
-        alert(err);
-      }
-      window.location = 'http://localhost:3000/login';
-    });
+  const response = await axios.get('http://localhost:2802/attacks', config);
+  return response.data;
+}
 
-  return returnData;
+// id, description, x_mitre_platforms, x_mitre_detection, phase_name
+// class Attacks extends React.Component
+const classes = 'useStyles()';
+
+export default class Attacks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { attacks: [] };
+  }
+
+  async componentDidMount() {
+    try {
+      getAttacks().then((res) => this.setState({ attacks: res }));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Title>Recent Attacks</Title>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>x_mitre_platforms</TableCell>
+              <TableCell>x_mitre_detection</TableCell>
+              <TableCell>phase_name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.attacks.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.id}</TableCell>
+                <TableCell>
+                  <ShowMoreText>{row.description}</ShowMoreText>
+                </TableCell>
+                <TableCell>{row.x_mitre_platforms.toString()}</TableCell>
+                <TableCell>
+                  width: 50
+                  <ShowMoreText>{row.x_mitre_detection}</ShowMoreText>
+                </TableCell>
+                <TableCell>{row.phase_name}</TableCell>
+                <TableCell align="right">{row.amount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <div className={classes.seeMore}>
+          <Link color="primary" href="#" onClick={preventDefault}>
+            See more attacks
+          </Link>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
