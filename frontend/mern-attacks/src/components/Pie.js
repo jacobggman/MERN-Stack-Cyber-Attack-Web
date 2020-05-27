866c4f0690db75805b9c61ec79837c61a64f2f0e
Jacob Galam <hgecdkto@gmail.com>
2020-05-27 21:13:24 +0300

    add title to charts names

--- frontend/mern-attacks/src/components/Pie.js
+++ frontend/mern-attacks/src/components/Pie.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import Chart from 'react-apexcharts';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import Title from './Title';
 
 class Donut extends Component {
   render() {
     return (
       <div className="donut">
         <Container>
-          <Row>{this.props.name}</Row>
+          <Row>
+            <Title>{this.props.name}</Title>
+          </Row>
           <Row></Row>
         </Container>
         <Chart
