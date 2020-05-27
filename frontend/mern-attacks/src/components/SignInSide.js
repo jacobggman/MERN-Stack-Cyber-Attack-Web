d045df706f25311dbda101bdf916cf839f006c23
Jacob Galam <hgecdkto@gmail.com>
2020-05-27 11:12:42 +0300

    remove forgot password

--- frontend/mern-attacks/src/components/SignInSide.js
+++ frontend/mern-attacks/src/components/SignInSide.js
@@ -109,11 +109,7 @@ export default function SignInSide() {
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="/forgot" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
+              <Grid item xs></Grid>
               <Grid item>
                 <Link href="/register" variant="body2">
                   {"Don't have an account? Sign Up"}
