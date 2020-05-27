1911a6b894267ed4471ea8b5a3ee7a1f451766c7
Jacob Galam <hgecdkto@gmail.com>
2020-05-27 22:29:30 +0300

    add default routing

--- frontend/mern-attacks/src/App.js
+++ frontend/mern-attacks/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import SignInSide from './components/SignInSide.js';
 import SignUp from './components/SignUp.js';
 import AttacksPage from './components/AttacksPage';
@@ -10,6 +10,8 @@ function App() {
     <Router>
       {/*<NavBar />*/}
       <br />
+      <Redirect from="/" to="login" />
+
       <Route path="/login" component={SignInSide} />
       <Route path="/register" component={SignUp} />
       <Route path="/attacks" component={AttacksPage} />
